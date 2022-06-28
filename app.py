f4972c7256bc46c9637dbb92fb0eaa941b664dc1
Mohamed Essam <imessam1998@gmail.com>
2022-06-28 02:22:41 +0200

    Add herouku config files

--- app.py
+++ app.py
@@ -6,23 +6,19 @@ from resources.user import RegisterUser
 from resources.item import Item,ItemList
 
 
-def start():
-    app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.secret_key = "hello"
-    api = Api(app)
-    jwt = JWT(app,authenticate,identity)
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = "hello"
+api = Api(app)
+jwt = JWT(app,authenticate,identity)
 
-    api.add_resource(Item,"/item/<string:name>")
-    api.add_resource(ItemList,"/items")
-    api.add_resource(RegisterUser,"/register")
-
-
-    app.run(port = 5000)
+api.add_resource(Item,"/item/<string:name>")
+api.add_resource(ItemList,"/items")
+api.add_resource(RegisterUser,"/register")
 
 
 if __name__ == "__main__":
     
 
-    start()
+    app.run(port = 5000)
