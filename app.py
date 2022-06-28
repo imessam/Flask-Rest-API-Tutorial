6134273a9f4a825dd8f46a3907faadbe49ee63ed
Mohamed Essam <imessam1998@gmail.com>
2022-06-29 01:36:15 +0200

    Update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,jsonify,request
 from flask_restful import Api
 from flask_jwt import JWT
@@ -7,13 +8,12 @@ from resources.item import Item,ItemList
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "hello"
 api = Api(app)
 jwt = JWT(app,authenticate,identity)
 
-    
 
 api.add_resource(Item,"/item/<string:name>")
 api.add_resource(ItemList,"/items")
