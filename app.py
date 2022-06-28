3b6e71966382b7c6b831f66bca05cef10c8e863e
Mohamed Essam <imessam1998@gmail.com>
2022-06-29 01:51:33 +0200

    Update

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 import os
-from flask import Flask,jsonify,request
+from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from security import authenticate,identity
+from security import authenticate, identity
 from resources.user import RegisterUser
-from resources.item import Item,ItemList
-
+from resources.item import Item, ItemList
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL","sqlite:///data.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db").replace('postgres://',
+                                                                                                    'postgresql://')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = "hello"
 api = Api(app)
-jwt = JWT(app,authenticate,identity)
+jwt = JWT(app, authenticate, identity)
 
-
-api.add_resource(Item,"/item/<string:name>")
-api.add_resource(ItemList,"/items")
-api.add_resource(RegisterUser,"/register")
-
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemList, "/items")
+api.add_resource(RegisterUser, "/register")
 
 if __name__ == "__main__":
-
-    app.run(port = 5000, debug = True)
+    app.run(port=5000, debug=True)
